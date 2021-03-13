cf7026d37a8cfc589422d62df5f88dbb3b5c94d1
mtdemircan <celticheroee@gmail.com>
2021-03-14 00:23:35 +0300

    koddaki değişilikler

--- app/src/main/java/WebAppProject481/App.java
+++ app/src/main/java/WebAppProject481/App.java
@@ -7,8 +7,20 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
+    
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
+
+    public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+  
+        for (int elt : array) {
+          if (elt == e) return true;
+        }
+        return false;
+      }
+
 }
